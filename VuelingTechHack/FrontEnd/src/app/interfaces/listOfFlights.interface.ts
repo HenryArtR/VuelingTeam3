26569024037d35de227228e1e5d47df7ff158a9e
HenryArtR <githubworkflowhenry@gmail.com>
2023-02-21 11:42:22 +0100

    interface y servicio de lista de vuelos

---
+++ VuelingTechHack/FrontEnd/src/app/interfaces/listOfFlights.interface.ts
@@ -0,0 +1,10 @@
+export interface ListOfFlights {
+  data_flight: string,
+  cd_flight_number: string,
+  cd_code_dep: string,
+  cd_code_arr: string,
+  handling_function: string,
+  ts_operation_start_utc: string,
+  ts_operation_end_utc: string,
+  required_employees: string
+}
--- VuelingTechHack/FrontEnd/src/app/services/mainservice.service.ts
+++ VuelingTechHack/FrontEnd/src/app/services/mainservice.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ListOfFlights } from '../interfaces/listOfFlights.interface';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MainserviceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getListofFlights(){
+    return this.http.get<ListOfFlights>('')
+  }
+
+  
+
 }
